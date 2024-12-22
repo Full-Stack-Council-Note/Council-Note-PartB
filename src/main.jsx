3b53be10fbdb05671c48934756e48aff23c435af
Robert Bailey <r.bail@hotmail.com>
2024-12-22 15:27:46 +1030

    fixing mainjs

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
 import './index.css';
-//import axios from 'axios';
+import axios from 'axios';
 import.meta.env.VITE_PORT
-//import.meta.env.REACT_APP_ENDPOINT
+import.meta.env.REACT_APP_ENDPOINT
                         
 //axios.defaults.baseURL = 'https://councilnote.netlify.app/';
 axios.defaults.REACT_APP_ENDPOINT = 'https://council-note-backend-5cf218cede7a.herokuapp.com/';
